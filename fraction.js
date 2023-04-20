060bdecd40e1f0b61b20c6dcb09ba8120e5aadb9
dharshan1916 <117891368+dharshan1916@users.noreply.github.com>
2023-04-20 11:59:33 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,18 @@
+class Multiplier {
+  constructor(x) {
+    this.x = x;
+  }
+  
+  multiply(y) {
+    return this.x * y;
+  }
+}
+
+// Create a new instance of the Multiplier class
+const multiplier = new Multiplier(5);
+
+// Multiply 7 by 5 using the multiply method
+const result = multiplier.multiply(7);
+
+console.log(result); // Output: 35
 
